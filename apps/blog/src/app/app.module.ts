42b839799ed816abdca5abfaec203b001a0d382d
Ramprit Sahani <rampritsahani@outlook.in>
2018-09-30 14:42:49 +0530

    universal added

--- apps/blog/src/app/app.module.ts
+++ apps/blog/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HomeModule } from './home/home.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     NxModule.forRoot(),
     HttpClientModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
