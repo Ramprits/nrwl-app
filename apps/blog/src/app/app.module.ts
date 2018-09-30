386e5efbfff19f3490f19a1b7f77b12991b638cf
Ramprit Sahani <rampritsahani@outlook.in>
2018-09-30 17:59:00 +0530

    path added

--- apps/blog/src/app/app.module.ts
+++ apps/blog/src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { HomeRoutingModule } from './home/home-routing.module';
-import { CoreModule } from '../../../../libs/core/src/lib/core.module';
+import { CoreModule } from '@Myworkspace/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeModule } from './home/home.module';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
